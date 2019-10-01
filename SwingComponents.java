08eff670f297ccc1514795a779c96d2b65ee2b97
MarybEdwards <44681953+MarybEdwards@users.noreply.github.com>
2019-10-01 17:14:52 -0400

    Update SwingComponents.java

--- SwingComponents.java
+++ SwingComponents.java
@@ -1,3 +1,16 @@
-public class SwingComponents {
+import java.awt.*;
+import javax.swing.*;
 
+
+public class SwingComponents { 
+ 	
+	public void createButton(){
+		JButton newButtonCreation = new JButton ("Click This");
+	}
+	
+	public void createScreen(){
+		JFrame thisWindow = new JFrame("Trial");	
+		thisWindow.visible(True);
+	}
+	
 }
